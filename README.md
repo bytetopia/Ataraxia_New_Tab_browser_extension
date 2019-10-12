c515f90ce8dd5d9709ed8d4c85b24a6a3744d2e3
anthony-mac <anthony@anthony-mac.local>
2019-10-12 21:57:54 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# Chrome-New-Tab-with-Bing-Wallpaper
+
+# 必应每日壁纸 -  Chrome 新标签页插件
+
+功能特性：
+
+- 自动同步必应每日壁纸
+- 支持多个搜索引擎
+- 极简
+
+[sample(images/sample.png)
+
+## 安装
+
+下载本仓库的 zip 文件，或者 git clone 仓库。
+
+打开 Chrome - 扩展程序，启用开发者模式，点击 `加载已解压的扩展程序`，选择本仓库下的 `extension` 目录即可
+
+## 使用说明
+
+开箱即用。
+
+点击搜索引擎的 logo，即可切换搜索引擎。插件会自动记住你当前选择的搜索引擎。
+
+## 自定义
+
+修改`blank.html` 文件，可以添加更多搜索引擎。
+
+修改搜索引擎个数后，请修改 `script.js` 顶部的 `totalEngines` 变量为最新个数。
+
+
+---
+
+
+# Chrome New Tab Extension with Bing Wallpaper
+
 Chrome new tab extension with Bing daily wallpaper as background.
+
+
+## Install
+
+Download zip or git clone this repository.
+
+Open `chrome://extensions/` , enable `Developer mode`. Click load unzipped extension button, select the `extension` folder.
+
+
+## Usage
+
+Just enjoy it.
+
+Click the search engine logo to switch between search engines.
+
+
+## Customize
+
+Modify `blank.html` to add new search engines.
+
+Modify the `totalEngines` variable at the top line of `script.js` if you add or delete some search engines.
+
+
+
+---
+
---
+++ extension/script.js
@@ -0,0 +1,70 @@
+
+var totalEngines = 5;
+
+function setCookieEngine(name){
+	document.cookie = "engine=" + name + ";expires=2147483647";
+}
+
+function getCookieEngine(){
+	var cookie = document.cookie;
+	if(cookie.indexOf("engine=") == -1){
+		return 0;
+	}
+    return parseInt(cookie.substring(cookie.indexOf("engine=")+7, cookie.indexOf("engine=")+8));
+}
+
+function updateWallpaper(){
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState==4){
+			var obj = JSON.parse(xhr.responseText);
+			var url = 'https://bing.com' + obj.images[0].url;
+			var body = document.getElementById('main-body');
+			body.style.backgroundImage = "url('" + url + "')";
+			var background_text = document.getElementById('background-text');
+			background_text.innerHTML = obj.images[0].copyright;
+		}
+	}
+	xhr.open('get','https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN');
+	xhr.send(null);
+}
+
+function switchEngine(){
+	var engine = getCookieEngine();
+	// hide old
+	var box = document.getElementById("search-box-" + engine);
+	box.style.display = "none";
+	// show new
+	engine = (engine + 1) % totalEngines;
+	box = document.getElementById("search-box-" + engine);
+	box.style.display = "inline-block";
+	setCookieEngine(engine);
+}
+
+function initEngine(){
+	var engine = getCookieEngine();
+	// hide others
+	for(var i=0; i < totalEngines; i++) {
+		if(i != engine){
+			var box = document.getElementById("search-box-" + i);
+			box.style.display = "none";
+		}
+	}
+	// show new
+	box = document.getElementById("search-box-" + engine);
+	box.style.display = "inline-block";
+}
+
+
+// init wallpaper
+updateWallpaper();
+
+// bind switch logo function to logo
+var searchLogos = document.getElementsByClassName('search-logo');
+for(var i=0; i<searchLogos.length; i++){
+	searchLogos[i].onclick = switchEngine;
+}
+
+// init engine
+initEngine();
+

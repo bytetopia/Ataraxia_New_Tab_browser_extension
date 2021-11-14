059d36a34b80f87469a5e59935b94b412314a21a
anthony <anthony@192.168.0.107>
2021-11-14 23:43:10 +0800

    fix

--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -5,7 +5,7 @@ chrome.runtime.onInstalled.addListener(function (object) {
 	// open manual link
 	if (chrome.runtime.OnInstalledReason.INSTALL === object.reason ) {
 		// open Welcome page
-		chrome.tabs.create({url: "https://ataraxia.dongxing.xin/install.html?utm_medium=chrome&utm_source=" + navigator.language}, function (tab) {
+		chrome.tabs.create({url: "https://ataraxia.dongxing.xin/install.html?utm_medium=edge&utm_source=" + navigator.language}, function (tab) {
 	        console.log("New tab launched with https://ataraxia.dongxing.xin/install.html");
 		});
 
@@ -13,4 +13,4 @@ chrome.runtime.onInstalled.addListener(function (object) {
 });
 
 // on uninstall
-chrome.runtime.setUninstallURL("https://ataraxia.dongxing.xin/uninstall.html?utm_medium=chrome&utm_source=" + navigator.language);
+chrome.runtime.setUninstallURL("https://ataraxia.dongxing.xin/uninstall.html?utm_medium=edge&utm_source=" + navigator.language);
